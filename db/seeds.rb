6d4a35676b051295173d982f9b75240444231711
Dan Nollette <nollette.dan@gmail.com>
2017-12-15 10:20:10 -0800

    add businesses to seed file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,46 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+class Seed
+
+  def self.begin
+    seed = Seed.new
+    seed.generate_businesses
+  end
+  Business.all.each do |b|
+    b.destroy
+  end
+  def generate_businesses
+    Business.create!(
+      name: "Chateau De JimBo",
+      address: "123 Fake St.",
+      description: "A fine French Dining establishment with all the best vittles."
+    )
+    Business.create!(
+      name: "Luther's pants",
+      address: "2188, Sycamore Avenue suite #7",
+      description: "Like pants? Need pants? got money for pants. Come on down to Luther's"
+    )
+    Business.create!(
+      name: "Post office",
+      address: ".... somewhere around here, I think",
+      description: "The place to be when you want to mail something"
+    )
+    Business.create!(
+      name: "Wildcard Comics",
+      address: "743 Evergreen Terrace",
+      description: "All the newest Comics and Games are at Wildcard. Note: cards not available."
+    )
+    Business.create!(
+      name: "Kwik-E-Mart",
+      address: "Over on the corner by the thing.",
+      description: "Swing on by, get a squishy, say Hi to Apu"
+    )
+    Business.all.each do |b|
+      puts "Business #{b.id}: name is '#{b.name}'' and address is '#{b.address}' and description is '#{b.description}'."
+    end
+  end
+end
+
+Seed.begin
