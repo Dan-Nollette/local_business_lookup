07967ff4423dcdddfc3e0c772c160cfc1a7a9b6c
Dan Nollette <nollette.dan@gmail.com>
2017-12-15 10:34:41 -0800

    Add controllers fix name or business controller file.

---
+++ app/controllers/businesses_controller.rb
@@ -0,0 +1,36 @@
+class BusinessesController < ApplicationController
+
+  def index
+    @businesses = Business.all
+    json_response(@businesses)
+  end
+
+  def show
+    @business = Business.find(params[:id])
+    json_response(@business)
+  end
+
+  def create
+    @business = Business.create(business_params)
+    json_response(@business)
+  end
+
+  def update
+    @business = Business.find(params[:id])
+    @business.update(business_params)
+  end
+
+  def destroy
+    @business = Business.find(params[:id])
+    @business.destroy
+  end
+
+  private
+  def json_response(object)
+    render json: object, status: :ok
+  end
+
+  def business_params
+    params.permit(:name, :description, :address)
+  end
+end
